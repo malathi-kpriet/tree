041a83495a56ff103556cf5a4098a5d49e17d1c7
malathi-kpriet <malathikanakaraj14@gmail.com>
2016-07-10 11:43:48 +0530

    finish

---
+++ README.c
@@ -0,0 +1,31 @@
+# tree
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+char name={"tree"};
+int thickness;
+int branches;
+char color[];
+int angle;
+printf("enter the thickness");
+scanf("%d,&thickness);
+printf("enter the branches);
+scanf("%d,&branches);
+printf("enter the color);
+scanf("%c",&color);
+printf("enter the angle");
+scanf("%d",&angle
+if(thickness>20)
+{
+if(branches>=10)
+{
+if(angle>15)
+{
+printf("\n tree angle is good");
+}
+printf("\n tree branch has many branches");
+}
+printf("\n the tree is thick");
+}
+}
